28b05cf7239f5b5172204203f63f011e1b56e46d
Takuya Yonezawa <yonezawa.takuya.yq2@gmail.com>
2023-06-24 06:52:57 +0900

    up

--- src/app/api/health/route.ts
+++ src/app/api/health/route.ts
@@ -1,7 +1,5 @@
 import { NextResponse } from 'next/server'
- 
-export async function GET() {
 
- 
-  return NextResponse.json({ message: 'health check OK' },{status:200})
+export async function GET() {
+  return NextResponse.json({ message: 'health check OK' }, { status: 200 })
 }
